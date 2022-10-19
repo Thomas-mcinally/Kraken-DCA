9c4ebc5db40de2b6af5913e6e7b88e82063ae19a
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-19 22:10:13 +0100

    make sure kraken api call is made

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+import requests
+
 def place_limit_order(ticker:str, amount_to_purchase:float, leverage:float, limit_price:float):
-    pass
+    requests.post("https://api.kraken.com/0/private/AddOrder")
--- test_main.py
+++ test_main.py
@@ -1,4 +1,16 @@
 from main import place_limit_order
+import pytest
+import responses
+
+@pytest.fixture
+def mocked_responses():
+    with responses.RequestsMock() as rsps:
+        yield rsps
 
 def test_input_arguments_exist():
+    place_limit_order(ticker="ETH-USD", amount_to_purchase=0.002, leverage=0.0, limit_price=129.1)
+
+def test_call_to_kraken_add_order_endpoint_is_made(mocked_responses):
+    mocked_responses.post("https://api.kraken.com/0/private/AddOrder")
+    
     place_limit_order(ticker="ETH-USD", amount_to_purchase=0.002, leverage=0.0, limit_price=129.1)
