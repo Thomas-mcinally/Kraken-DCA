3a53b6e0413d8f3d2628e3f133cad6947f453463
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-19 21:52:14 +0100

    fourth argument

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-def place_limit_order(ticker:str, amount_to_purchase:float, leverage:float):
+def place_limit_order(ticker:str, amount_to_purchase:float, leverage:float, limit_price:float):
     pass
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
 from main import place_limit_order
 
 def test_input_arguments_exist():
-    place_limit_order(ticker="ETH-USD", amount_to_purchase=0.002, leverage=0.0)
+    place_limit_order(ticker="ETH-USD", amount_to_purchase=0.002, leverage=0.0, limit_price=129.1)
