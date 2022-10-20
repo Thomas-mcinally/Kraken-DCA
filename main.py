a10255b9e375027433fc1a85b453e34b7f741e16
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-20 07:49:48 +0100

    use price from GET call to make limit order

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ def place_limit_order(ticker:str, eur_budget:float):
                 "ordertype": "limit",
                 "type": "buy",
                 "volume": "0.002",
-                "price": "19200.1",
+                "price": str(bid_price),
                 "pair": ticker
         }
     )
 
 def get_bid_price(ticker:str) -> float:
-    market_data = requests.get(url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}")
-    return float(0)
+    market_data:dict[str, dict[str, dict[str, list[str]]]] = requests.get(url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}").json()
+    top_market_bid = float(market_data["result"][ticker]["b"][0])
+    my_bid_price = round(top_market_bid, 6)
+    return my_bid_price
 
--- test_main.py
+++ test_main.py
@@ -8,10 +8,73 @@ def mocked_responses():
     with responses.RequestsMock() as rsps:
         yield rsps
 
-@pytest.mark.parametrize("ticker",[("ETHUSD"),("XXBTZUSD")])
-def test_call_to_kraken_add_order_endpoint_is_made(mocked_responses, ticker):
+@pytest.mark.parametrize("ticker, current_market_bid, expected_bid_price ",[("ETHUSD", "192.125678123", "192.125678"),("XXBTZUSD", "19200.125678123", "19200.125678")])
+def test_call_to_kraken_add_order_endpoint_is_made(mocked_responses, ticker, current_market_bid, expected_bid_price):
     mocked_responses.get(
-        url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}"
+        url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}",
+        json=
+            {
+  "result": {
+    ticker: {
+      "a": [
+        "string"
+      ],
+      "b": [
+        current_market_bid
+      ],
+      "c": [
+        "string"
+      ],
+      "v": [
+        "string"
+      ],
+      "p": [
+        "string"
+      ],
+      "t": [
+        0
+      ],
+      "l": [
+        "string"
+      ],
+      "h": [
+        "string"
+      ],
+      "o": "string"
+    },
+    "pair2": {
+      "a": [
+        "string"
+      ],
+      "b": [
+        "string"
+      ],
+      "c": [
+        "string"
+      ],
+      "v": [
+        "string"
+      ],
+      "p": [
+        "string"
+      ],
+      "t": [
+        0
+      ],
+      "l": [
+        "string"
+      ],
+      "h": [
+        "string"
+      ],
+      "o": "string"
+    }
+  },
+  "error": [
+    "EGeneral:Invalid arguments"
+  ]
+}
+        
     )
     mocked_responses.post(
         url = "https://api.kraken.com/0/private/AddOrder",
@@ -20,7 +83,7 @@ def test_call_to_kraken_add_order_endpoint_is_made(mocked_responses, ticker):
                 "ordertype": "limit",
                 "type": "buy",
                 "volume": "0.002",
-                "price": "19200.1",
+                "price": expected_bid_price,
                 "pair": ticker
             })
         ]
