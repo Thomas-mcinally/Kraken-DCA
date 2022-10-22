d7bff11be54a2b1b8de298db7160f03a0e05b743
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-22 17:25:10 +0100

    make POST request body URL-encoded instead odf JSON-encoded

--- main.py
+++ main.py
@@ -3,31 +3,33 @@ import hashlib
 import hmac
 import math
 import time
-import requests
-import pytest
 
-def place_limit_order(ticker:str, eur_budget:float, private_key:str, public_key:str):
+import requests
+
+
+def place_limit_order(ticker:str, budget:float, private_key:str, public_key:str):
 
     bid_price:str = get_bid_price(ticker)
-    volume:str = get_trade_volume(eur_budget, bid_price)
+    volume:str = get_trade_volume(budget, bid_price)
     nonce:int = int(time.time()*1000)
-    api_sign = get_api_sign(nonce=str(nonce), ticker=ticker, bid_price=bid_price, volume=volume, private_key=private_key)
-    
+    api_sign:str = get_api_sign(nonce=str(nonce), ticker=ticker, bid_price=bid_price, volume=volume, private_key=private_key)
+
     requests.post(
         url="https://api.kraken.com/0/private/AddOrder",
-        json={
+        data={
+                "nonce": nonce,
                 "ordertype": "limit",
+                "pair": ticker,
+                "price": bid_price,
                 "type": "buy",
                 "volume": volume,
-                "price": bid_price,
-                "pair": ticker,
-                "nonce": nonce
         },
         headers={
             "API-Key": public_key,
             "API-Sign": api_sign
         }
     )
+
 def round_down(n:float, decimals=0):
     multiplier = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
@@ -35,6 +37,7 @@ def round_down(n:float, decimals=0):
 def get_api_sign(nonce:str, ticker:str, bid_price:str, volume:str, private_key:str) ->str:
     api_path = '/0/private/AddOrder'
     api_post=f'nonce={nonce}&ordertype=limit&pair={ticker}&price={bid_price}&type=buy&volume={volume}'
+
     api_sha256 = hashlib.sha256(nonce.encode('utf8') + api_post.encode('utf8'))
     api_hmac = hmac.new(base64.b64decode(private_key), api_path.encode('utf8') + api_sha256.digest(), hashlib.sha512)
     api_signature:bytes = base64.b64encode(api_hmac.digest())
@@ -51,4 +54,4 @@ def get_bid_price(ticker:str) -> str:
 def get_trade_volume(budget:float, bid_price:str) -> str:
     return str(budget/float(bid_price))
 
-
+# TODO: Am i passing arguments correctly? Should it be in body or in a header??
--- test_main.py
+++ test_main.py
@@ -9,13 +9,13 @@ def mocked_responses():
         yield rsps
 
 @pytest.mark.parametrize(
-    "ticker, current_market_bid, eur_budget, current_time, expected_nonce, expected_bid_price, expected_trade_volume",
+    "ticker, current_market_bid, budget, current_time, expected_nonce, expected_bid_price, expected_trade_volume",
     [
-        ("ETHUSD", "192.125678723", 11, 222.222, 222222, "192.125678", "0.05725418962477259"),
-        ("XXBTZUSD", "19200.125678723", 0.5, 111.111, 111111, "19200.125678", "2.6041496206085407e-05"),
+        ("ETHUSD", "192.125678723", 11, 222.222, "222222", "192.125678", "0.05725418962477259"),
+        ("XXBTZUSD", "19200.125678723", 0.5, 111.111, "111111", "19200.125678", "2.6041496206085407e-05"),
     ]
 )
-def test_calls_to_kraken_endpoints_are_made_with_values_calculated_from_inputs(mocked_responses, mocker, ticker, current_market_bid, eur_budget, current_time, expected_nonce, expected_bid_price, expected_trade_volume):
+def test_calls_to_kraken_endpoints_are_made_with_values_calculated_from_inputs(mocked_responses, mocker, ticker, current_market_bid, budget, current_time, expected_nonce, expected_bid_price, expected_trade_volume):
     mocked_responses.get(
         url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}",
         json={
@@ -31,29 +31,29 @@ def test_calls_to_kraken_endpoints_are_made_with_values_calculated_from_inputs(m
     mocked_responses.post(
         url = "https://api.kraken.com/0/private/AddOrder",
         match = [
-            matchers.json_params_matcher({
+            matchers.urlencoded_params_matcher({
+                "nonce": expected_nonce,
                 "ordertype": "limit",
-                "type": "buy",
-                "volume": expected_trade_volume,
-                "price": expected_bid_price,
                 "pair": ticker,
-                "nonce": expected_nonce
+                "price": expected_bid_price,
+                "type": "buy",
+                "volume": expected_trade_volume
             })
         ]
         )
     mocker.patch("time.time", return_value=current_time)
-    place_limit_order(ticker=ticker, eur_budget=eur_budget, private_key="kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==", public_key="111")
+    place_limit_order(ticker=ticker, budget=budget, private_key="kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==", public_key="111")
 
 
 
 @pytest.mark.parametrize(
-    "ticker, current_market_bid, eur_budget, current_time, private_key, expected_api_Sign, public_key",
+    "ticker, current_market_bid, budget, current_time, private_key, expected_api_Sign, public_key",
     [
         ("ETHUSD", "192.125678723", 11, 1616492376.594, "kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==", "l8th/OmRDGpwfHAoOCIuZF+c9X37krK1KELhRUoEcQU2Si3gHtCY/mPeIgw7saRe1fREa6vfMDIUSEs6ov+fEQ==", "fake111"),
         ("XXBTZUSD", "19200.125678723", 0.5, 1111111111.594, "111111/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==", "HxAjpPcHJZDU3dRdaBtZ312pqV2pD3zaeglYxpmeEjgVUUlJ62m8QQbjjmyVm2X+gc491tCdwKAIW1ACJJxvGA==", "fake222"),
     ]
 )
-def test_call_to_private_kraken_endpoint_is_made_with_required_headers_and_nonce_is_included_in_body(mocked_responses, mocker, ticker, current_market_bid, eur_budget, current_time, private_key, expected_api_Sign, public_key):
+def test_call_to_private_kraken_endpoint_is_made_with_required_headers_and_nonce_is_included_in_body(mocked_responses, mocker, ticker, current_market_bid, budget, current_time, private_key, expected_api_Sign, public_key):
     mocked_responses.get(
         url=f"https://api.kraken.com/0/public/Ticker?pair={ticker}",
         json={
@@ -78,10 +78,4 @@ def test_call_to_private_kraken_endpoint_is_made_with_required_headers_and_nonce
 
     mocker.patch("time.time", return_value=current_time)
 
-    place_limit_order(ticker=ticker, eur_budget=eur_budget, private_key=private_key, public_key=public_key)
-
-
-
-# list of specs:
-# - Header called "API-Key" should contain api public key
-
+    place_limit_order(ticker=ticker, budget=budget, private_key=private_key, public_key=public_key)
