75325c1ac887496bfc78a817f0fedc441ad8ba72
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-23 13:51:30 +0100

    schedule lambda function with eventbridge

--- main.tf
+++ main.tf
@@ -104,3 +104,21 @@ resource "aws_lambda_function" "btc-dca-lambda" {
   role = aws_iam_role.iam-for-lambda.arn
 }
 
+resource "aws_cloudwatch_event_rule" "kraken_dca_lambda_event_rule" {
+  name = "kraken_dca_lambda_event_rule"
+  description = "execute event every day at 10:00 UTC"
+  schedule_expression = "cron(0 10 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "kraken_dca_lambda_event_target" {
+  arn = aws_lambda_function.btc-dca-lambda.arn
+  rule = aws_cloudwatch_event_rule.kraken_dca_lambda_event_rule.name
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_rw_fallout_retry_step_deletion_lambda" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.btc-dca-lambda.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.kraken_dca_lambda_event_rule.arn
+}
