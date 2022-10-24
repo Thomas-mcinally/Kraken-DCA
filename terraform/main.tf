d37a3c8f42e4fd1782c35705f9b8bd0df6f97c0e
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-24 17:38:44 +0100

    fix github action

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,14 +72,14 @@ data "archive_file" "dependencies_zip" {
   type = "zip"
 
   source_dir  = "./${path.module}/../dependencies"
-  output_path = "./${path.module}/../layer.zip"
+  output_path = "./${path.module}/layer.zip"
 }
 
 data "archive_file" "source_code_zip" {
   type = "zip"
 
   source_file  = "./${path.module}/../btc_dca_script.py"
-  output_path = "./${path.module}/../python_code.zip"
+  output_path = "./${path.module}/python_code.zip"
 }
 
 resource "aws_lambda_layer_version" "kraken_dca_dependencies" {
