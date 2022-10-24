a1064998948ee8b8c2958b1f582c9a5b00b1182c
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-24 18:04:01 +0100

    refactor - make get_aws_ssm_securestring_parameter function

--- btc_dca_script.py
+++ btc_dca_script.py
@@ -31,15 +31,15 @@ def place_limit_order(trading_pair:str, budget:float, private_key:str, public_ke
     return response
 
 def round_down(n:float, decimals:int) -> float:
-    multiplier = 10 ** decimals
+    multiplier:int = 10 ** decimals
     return math.floor(n * multiplier) / multiplier
 
 def get_api_sign(nonce:str, trading_pair:str, bid_price:str, volume:str, private_key:str) ->str:
     api_path = '/0/private/AddOrder'
     api_post=f'nonce={nonce}&ordertype=limit&pair={trading_pair}&price={bid_price}&type=buy&volume={volume}'
 
-    api_sha256 = hashlib.sha256(nonce.encode() + api_post.encode())
-    api_hmac = hmac.new(base64.b64decode(private_key), api_path.encode() + api_sha256.digest(), hashlib.sha512)
+    api_sha256:hashlib._Hash = hashlib.sha256(nonce.encode() + api_post.encode())
+    api_hmac:hmac.HMAC = hmac.new(base64.b64decode(private_key), api_path.encode() + api_sha256.digest(), hashlib.sha512)
     api_signature:bytes = base64.b64encode(api_hmac.digest())
     api_signature_decoded:str = api_signature.decode()
     return api_signature_decoded
@@ -48,17 +48,20 @@ def get_bid_price(trading_pair:str) -> str:
     market_data:dict = requests.get(url=f"https://api.kraken.com/0/public/Ticker?pair={trading_pair}").json()
     top_market_bid = float(market_data["result"][trading_pair]["b"][0])
     my_bid_price = str(round_down(top_market_bid, decimals=6))
-    
     return my_bid_price
 
 def get_trade_volume(budget:float, bid_price:str) -> str:
     return str(budget/float(bid_price))
 
-def lambda_handler(event, context):
+def get_aws_ssm_securestring_parameter(paramname:str) -> str:
     client = boto3.client('ssm')
-    budget:float = float(client.get_parameter(Name="kraken-dca-BTC-daily-purchase-amount", WithDecryption=True)["Parameter"]["Value"])
-    private_key:str = client.get_parameter(Name="kraken-private-api-key", WithDecryption=True)["Parameter"]["Value"]
-    public_key:str = client.get_parameter(Name="kraken-public-api-key", WithDecryption=True)["Parameter"]["Value"]
+    securestring = client.get_parameter(Name=paramname, WithDecryption=True)["Parameter"]["Value"]
+    return securestring
+
+def lambda_handler(event, context):
+    budget:float = float(get_aws_ssm_securestring_parameter("kraken-dca-BTC-daily-purchase-amount"))
+    private_key:str = get_aws_ssm_securestring_parameter("kraken-private-api-key")
+    public_key:str = get_aws_ssm_securestring_parameter("kraken-public-api-key")
     
     response = place_limit_order(trading_pair='XXBTZEUR', budget=budget, private_key=private_key, public_key=public_key)
     
