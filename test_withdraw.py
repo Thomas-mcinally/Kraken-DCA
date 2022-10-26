dd6d19462177b11cf6f702130439373a434dc5cb
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-26 09:04:37 +0100

    reformat

--- test_withdraw.py
+++ test_withdraw.py
@@ -3,6 +3,7 @@ import responses
 from responses import matchers
 from withdraw import withdraw_crypto_from_kraken
 
+
 @pytest.fixture
 def mocked_responses():
     with responses.RequestsMock() as rsps:
@@ -12,53 +13,46 @@ def mocked_responses():
 @pytest.mark.parametrize(
     "current_time, expected_nonce, asset_to_withdraw, withdrawal_address_key",
     [
-        (111.111, '111111', 'XXBTZ', 'btc_hardwallet'),
-        (222.222, '222222', 'ETH', 'eth_hardwallet')
+        (111.111, "111111", "XXBTZ", "btc_hardwallet"),
+        (222.222, "222222", "ETH", "eth_hardwallet"),
     ],
 )
 def test_calls_to_kraken_balance_and_withdraw_endpoints_are_made(
-    mocked_responses, 
-    mocker, 
-    current_time, 
+    mocked_responses,
+    mocker,
+    current_time,
     expected_nonce,
     asset_to_withdraw,
-    withdrawal_address_key):
+    withdrawal_address_key,
+):
     mocked_responses.post(
-        url='https://api.kraken.com/0/private/Balance',
-        match=[
-            matchers.urlencoded_params_matcher(
-                {
-                    'nonce': expected_nonce
-                }
-            )
-        ],
-        json={
-            "result": {
-                "ZUSD": "2970172.7962"
-            },
-            "error": []
-        }
+        url="https://api.kraken.com/0/private/Balance",
+        match=[matchers.urlencoded_params_matcher({"nonce": expected_nonce})],
+        json={"result": {"ZUSD": "2970172.7962"}, "error": []},
     )
-    
+
     mocked_responses.post(
-        url='https://api.kraken.com/0/private/Withdraw',
+        url="https://api.kraken.com/0/private/Withdraw",
         match=[
             matchers.urlencoded_params_matcher(
                 {
-                    'nonce': expected_nonce,
-                    'asset': asset_to_withdraw,
-                    'key': withdrawal_address_key
+                    "nonce": expected_nonce,
+                    "asset": asset_to_withdraw,
+                    "key": withdrawal_address_key,
                 }
             )
         ],
     )
     mocker.patch("time.time", return_value=current_time)
 
-    withdraw_crypto_from_kraken(asset_to_withdraw=asset_to_withdraw, withdrawal_address_key=withdrawal_address_key)
+    withdraw_crypto_from_kraken(
+        asset_to_withdraw=asset_to_withdraw,
+        withdrawal_address_key=withdrawal_address_key,
+    )
 
 
-
-
-def test_calls_to_kraken_balance_and_withdraw_endpoints_are_made_with_different_nonces(mocked_responses):
+def test_calls_to_kraken_balance_and_withdraw_endpoints_are_made_with_different_nonces(
+    mocked_responses,
+):
     # FIGURE OUT HOW TO TEST THIS LATER, CURRENTLY USING SAME NONCE FOR BOTH CALLS
     pass
--- withdraw.py
+++ withdraw.py
@@ -2,11 +2,14 @@ import time
 import requests
 
 
-def withdraw_crypto_from_kraken(asset_to_withdraw:str, withdrawal_address_key:str):
+def withdraw_crypto_from_kraken(asset_to_withdraw: str, withdrawal_address_key: str):
     nonce: int = int(time.time() * 1000)
+    requests.post(url="https://api.kraken.com/0/private/Balance", data={"nonce": nonce})
     requests.post(
-        url='https://api.kraken.com/0/private/Balance',
-        data={'nonce':nonce}
+        url="https://api.kraken.com/0/private/Withdraw",
+        data={
+            "nonce": nonce,
+            "asset": asset_to_withdraw,
+            "key": withdrawal_address_key,
+        },
     )
-    requests.post(url='https://api.kraken.com/0/private/Withdraw', data={'nonce':nonce, 'asset': asset_to_withdraw, 'key':withdrawal_address_key})
-
