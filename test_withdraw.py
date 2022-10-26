b44092b58d8dd7a421d61eb22340b9e06c83c092
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-10-26 08:35:30 +0100

    include nonce in body of call to kraken balances endpoint

--- test_withdraw.py
+++ test_withdraw.py
@@ -1,5 +1,6 @@
 import pytest
 import responses
+from responses import matchers
 from withdraw import withdraw_crypto_from_kraken
 
 @pytest.fixture
@@ -10,6 +11,13 @@ def mocked_responses():
 
 def test_call_to_kraken_balance_endpoint_is_made(mocked_responses):
     mocked_responses.post(
-        url='https://api.kraken.com/0/private/Balance'
+        url='https://api.kraken.com/0/private/Balance',
+        match=[
+            matchers.urlencoded_params_matcher(
+                {
+                    'nonce': '123'
+                }
+            )
+        ]
     )
     withdraw_crypto_from_kraken()
--- withdraw.py
+++ withdraw.py
@@ -2,4 +2,7 @@ import requests
 
 
 def withdraw_crypto_from_kraken():
-    requests.post(url='https://api.kraken.com/0/private/Balance')
+    requests.post(
+        url='https://api.kraken.com/0/private/Balance',
+        data={'nonce':'123'}
+    )
