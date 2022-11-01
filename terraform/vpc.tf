91e8ec3646d9f52f87ed12d0306407a20dcfd5e5
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-01 19:58:52 +0000

    associate route tables with subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -68,3 +68,12 @@ resource "aws_route" "private_nat_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
+/* Route table associations */
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_subnet_route_table.id
+}
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_subnet_route_table.id
+}
