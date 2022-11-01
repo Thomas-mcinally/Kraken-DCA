cea20950bbd37e7134e7611420135d83d4dad3be
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-01 19:12:54 +0000

    add private and public subnets to vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,23 @@
-resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/28"
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.100.0.0/16"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.100.0.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Kraken DCA private Subnet"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.100.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Kraken DCA public Subnet"
+  }
 }
