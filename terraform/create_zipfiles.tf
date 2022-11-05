cb88ced454670b11f42db6225676d3e63f284d09
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-05 19:02:44 +0100

    refactor - make separate directory for python scripts

--- terraform/create_zipfiles.tf
+++ terraform/create_zipfiles.tf
@@ -8,13 +8,13 @@ data "archive_file" "dependencies_zip" {
 data "archive_file" "source_code_zip" {
   type = "zip"
 
-  source_file = "./${path.module}/../dca.py"
+  source_file = "./${path.module}/../python_scripts/dca.py"
   output_path = "./${path.module}/python_code.zip"
 }
 
 data "archive_file" "withdraw_source_code_zip" {
   type = "zip"
 
-  source_file = "./${path.module}/../withdraw.py"
+  source_file = "./${path.module}/../python_scripts/withdraw.py"
   output_path = "./${path.module}/kraken_withdraw_python_code.zip"
 }
