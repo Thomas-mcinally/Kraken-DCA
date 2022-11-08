9a299c249804485923a17b17b5b3b957482c0100
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-08 21:52:00 +0000

    debug ec2 nat instance

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -6,12 +6,28 @@ resource "aws_eip" "nat_eip" {
 resource "aws_instance" "ec2_nat_instance" {
   ami           = "ami-0bbb886f29931c526"
   instance_type = "t3.nano"
-  subnet_id     = aws_subnet.public_subnet.id
   network_interface {
     network_interface_id = aws_network_interface.nat_ec2_network_interface.id
     device_index = 0
   }
+  user_data = <<EOT
+#!/bin/bash
+sudo /usr/bin/apt update
+sudo /usr/bin/apt install ifupdown
+/bin/echo '#!/bin/bash
+if [[ $(sudo /usr/sbin/iptables -t nat -L) != *"MASQUERADE"* ]]; then
+  /bin/echo 1 > /proc/sys/net/ipv4/ip_forward
+  /usr/sbin/iptables -t nat -A POSTROUTING -s ${aws_vpc.vpc.cidr_block} -j MASQUERADE
+fi
+' | sudo /usr/bin/tee /etc/network/if-pre-up.d/nat-setup
+sudo chmod +x /etc/network/if-pre-up.d/nat-setup
+sudo /etc/network/if-pre-up.d/nat-setup 
+  EOT
 
+  tags = {
+    Name = "nat_instance"
+    Role = "nat"
+  }
 }
 resource "aws_network_interface" "nat_ec2_network_interface" {
   subnet_id = aws_subnet.public_subnet.id
