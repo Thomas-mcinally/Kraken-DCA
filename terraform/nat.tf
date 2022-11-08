54c98335a30aff006e17954105e5d176bbb5ef6e
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-08 21:45:29 +0000

    add security group to nat instance

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -5,14 +5,49 @@ resource "aws_eip" "nat_eip" {
 }
 resource "aws_instance" "ec2_nat_instance" {
   ami           = "ami-0bbb886f29931c526"
-  instance_type = "t3.micro"
+  instance_type = "t3.nano"
   subnet_id     = aws_subnet.public_subnet.id
+  network_interface {
+    network_interface_id = aws_network_interface.nat_ec2_network_interface.id
+    device_index = 0
+  }
+
 }
 resource "aws_network_interface" "nat_ec2_network_interface" {
   subnet_id = aws_subnet.public_subnet.id
-
-  attachment {
-    instance     = aws_instance.ec2_nat_instance.id
-    device_index = 1
-  }
+  security_groups = [aws_security_group.security_group.id]
+  source_dest_check = false
+}
+resource "aws_security_group" "security_group" {
+  name = "nat_instance_security_group"
+  description = "Security group for NAT instance"
+  vpc_id = aws_vpc.vpc.id
+  
+  ingress = [
+    {
+      description = "Ingress CIDR"
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [aws_vpc.vpc.cidr_block]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = true
+    }
+  ]
+  
+  egress = [
+    {
+      description = "Default egress"
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      security_groups = []
+      self = true
+    }
+  ]
 }
